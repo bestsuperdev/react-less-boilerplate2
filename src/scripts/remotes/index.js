16b6dde91711f272d2d6a2d746dc8986816c8fb3
mingzepeng <mingzepeng@gmail.com>
2016-11-24 13:23:20 +0800

    add apibasepath

--- src/scripts/remotes/index.js
+++ src/scripts/remotes/index.js
@@ -1,7 +1,7 @@
 const {remote} = require('beyond-remote')
-
+const apiBasePath = typeof window !== 'undefined' && window.app && window.app.apiBasePath ? window.app.apiBasePath : ''
 remote.base({
-	basePath : '/api'
+	basePath : apiBasePath
 })
 
 export const getTodos = remote.create({url : '/todos'})
