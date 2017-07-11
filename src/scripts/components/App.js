beec51e980331b806d8e1545977705e79e9f4930
mingzepeng <mingzepeng@gmail.com>
2017-07-11 16:52:12 +0800

    update app.js

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -1,25 +1,14 @@
 import React from 'react'
-// import {getTodos} from 'scripts/remotes'
 export default class App extends React.Component {
 
 	constructor(props){
 		super(props)
-		this.state = {
-			todos : []
-		}
-	}
-
-	componentDidMount() {
-		// getTodos()
-		// .then((todos)=>{
-			// this.setState({todos})
-		// })
 	}
 
 	render() {
 		return (
 			<div className='app'>
-				please enjoy it ~
+				please enjoy it~
 			</div>
 		)
 	}
